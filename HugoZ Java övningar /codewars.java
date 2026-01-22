f09ffaa66cc826046d590d87fc9e5c8bb5f07ece
HugoZedendahl <70585483+HugoZedendahl@users.noreply.github.com>
2026-01-22 10:28:49 +0100

    added class with code i like

--- HugoZ Java övningar /codewars.java
+++ HugoZ Java övningar /codewars.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class övningEtt {
+public class codewars {
 
     public static void main(String[] arr) {
         int[] myArr = { 1, 2, 3, 5, 6, 4, 3, 5, 3, 23, 5, 3, 3 };
@@ -30,5 +30,20 @@ public class övningEtt {
             output += x[i];
         }
         return output / x.length;
+    }
+}
+
+
+//best practive switch strings
+
+class Solution {
+    public static String whoLikesIt(String... names) {
+        switch (names.length) {
+          case 0: return "no one likes this";
+          case 1: return String.format("%s likes this", names[0]);
+          case 2: return String.format("%s and %s like this", names[0], names[1]);
+          case 3: return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+          default: return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
+        }
     }
 }
