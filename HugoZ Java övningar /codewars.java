31297432e0b269e6177442e5fcda4131675e436f
HugoZedendahl <70585483+HugoZedendahl@users.noreply.github.com>
2026-01-22 11:31:26 +0100

    more stream solutions to analyze

--- HugoZ Java övningar /codewars.java
+++ HugoZ Java övningar /codewars.java
@@ -47,3 +47,14 @@ class Solution {
         }
     }
 }
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Order {
+  public static String order(String words) {
+    return Arrays.stream(words.split(" "))
+      .sorted(Comparator.comparing(s -> Integer.valueOf(s.replaceAll("\\D", ""))))
+      .reduce((a, b) -> a + " " + b).get();
+  }
+}
